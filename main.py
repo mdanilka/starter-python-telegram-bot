762d59fa8de06bc455b3df8fbf2d9e3d507d4b6d
mdanilka <136491815+mdanilka@users.noreply.github.com>
2024-01-24 22:05:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def auth_telegram_token(x_telegram_bot_api_secret_token: str = Header(None)) -> 
 @app.post("/webhook/")
 async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_telegram_token)):
     chat_id = update.message["chat"]["id"]
-    text = update.message["text", "photo"]
+    text = update.message["photo"]
     photo = update.message["photo"]
     # print("Received message:", update.message)
 
