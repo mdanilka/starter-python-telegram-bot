b263abd66c324c0944255b98ae219baa0ee6c85c
mdanilka <136491815+mdanilka@users.noreply.github.com>
2024-01-27 19:40:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Header, HTTPException, Depends
 from telegram import Update, Bot
 from pydantic import BaseModel
-
+from telebot import types
 class TelegramUpdate(BaseModel):
     update_id: int
     message: dict
